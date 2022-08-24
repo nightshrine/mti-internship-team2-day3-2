7cb5de7bb40e3f6fb782c8cf0cec0fe7cd0ed658
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2022-08-24 19:39:23 +0900

    feat: 記事を全て持ってくるところまで

---
+++ lambda/yamamoto-s-article-create-internship.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const tableName = "Article";
+
+exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ message: "" }),
+  };
+
+  const body = JSON.parse(event.body);
+
+  //TODO: DBに登録するための情報をparamオブジェクトとして宣言する（中身を記述）
+  const param = {
+    TableName: tableName,
+    Item: {
+      userId : body.userId,
+      timestamp : body.timestamp,
+      text : body.text,
+      category : body.category,
+    }
+  };
+
+  //dynamo.put()でDBにデータを登録
+  dynamo.put(param, function (err, data) {
+    if (err) {
+      console.log(err);
+      response.statusCode = 500;
+      response.body = JSON.stringify({
+        message: "予期せぬエラーが発生しました",
+        err: err
+      });
+      callback(null, response);
+      return;
+    } else {
+      //TODO: 登録に成功した場合の処理を記述
+      response.body = JSON.stringify({
+        message: "登録成功しました"
+      })
+      callback(null, response);
+      return;
+    }
+  });
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,73 @@
     <div class="ui main container">
       <!-- 基本的なコンテンツはここに記載する -->
       <div class="ui segment">
+        <form class="ui form" @submit.prevent="postArticle">
+          <div class="field">
+            <textarea type="text" placeholder="記事" v-model="post.text" >
+            </textarea>
+          </div>
+          <div class="field" style="align-items: center;">
+            <div class="inline fields">
+              <div class="field">
+                <label>カテゴリー</label>
+                <input type="text" v-model="post.category" />
+              </div>
+              <button class="ui button" type="submit">
+                投稿
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
+      <div class="ui segment">
+        <div class="field">
+          <form class="ui form" @submit.prevent="searchArticle">
+              <div class="field">
+                <label>ユーザー名</label>
+                <input type="text" v-model="search.text" />
+              </div>
+              <div class="field">
+                <label>カテゴリー名</label>
+                <input type="text" v-model="search.text" />
+              </div>
+              <div class="field">
+                <label>投稿日時</label>
+                <div class="inline fields">
+                  <div class="field">
+                    <input type="date" v-model="search.text" />
+                    <label>から</label>
+                  </div>
+                  <div class="field">
+                    <input type="date" v-model="search.text" />
+                    <label>まで</label>
+                  </div>
+                </div>
+              </div>
+            <button class="ui button" type="submit" style="width: 100%">検索</button>
+          </form>
+        </div>
+      </div>
+      <template v-for="(article, index) in sortedArticles">
+        <div class="ui segment" :key="index">
+          <form class="ui form">
+            <h2 class="ui header">
+              <div class="content">
+                {{article.userId}}
+                 <button class="ui red label" type="submit" >削除</button>
+              </div>
+              <div class="sub header">{{article.text}}</div>
+            </h2>
+            <div class="ui green label">{{article.category}}</div>
+          </form>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
-// import { baseUrl } from '@/assets/config.js';
-// import axios from "axios";
+import { baseUrl } from '@/assets/config.js';
+import axios from "axios";
 // 必要なものはここでインポートする
 // @は/srcと同じ意味です
 // import something from '@/components/something.vue';
@@ -18,7 +77,7 @@
 export default {
   name: 'Home',
   components: {
-   // 読み込んだコンポーネント名をここに記述する
+    // 読み込んだコンポーネント名をここに記述する
   },
   data() {
     // Vue.jsで使う変数はここに記述する
@@ -45,10 +104,13 @@ export default {
   },
   computed: {
   // 計算した結果を変数として利用したいときはここに記述する
-    /*
     sortedArticles() {
-    }
-    */
+      let result = this.articles;
+      // if(this.query.start && this.query.end) {
+      //   serachArticle();
+      // }
+      return result;
+    },
   },
   created() {
     // Vue.jsの読み込みが完了したときに実行する処理はここに記述する
@@ -57,12 +119,20 @@ export default {
     if (!window.localStorage.getItem("userId")) {
       this.$router.push({name: "Login"});
     }
+    this.getArticles();
   },
   methods: {
     // Vue.jsで使う関数はここで記述する
-    /*
     getArticles() {
+      axios.get(baseUrl + "/articles")
+        .then((res) => {
+          this.articles = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
+    /*
     postArticle() {
     },
     searchArticle() {
